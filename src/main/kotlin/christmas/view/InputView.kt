0b3886500cd513a63c193f2dd27d3b50b15de8b6
Namyunsuk <yunsuk0328@naver.com>
2023-11-10 17:27:03 +0900

    style: line formatting

--- src/main/kotlin/christmas/view/InputView.kt
+++ src/main/kotlin/christmas/view/InputView.kt
@@ -8,6 +8,7 @@ class InputView {
         val input = Console.readLine()
         return input.toInt()
     }
+
     fun readMenu(): Map<String, Int> {
         println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
         val input = Console.readLine()
