4e52dc810576dc5ebe258d01a075e0635a9ca7ce
Namyunsuk <yunsuk0328@naver.com>
2023-11-10 23:22:31 +0900

    feat(Promotion): 날짜, 요일, 메뉴에 따라 혜택 구분하여 적용하는 기능 구현

---
+++ src/main/kotlin/christmas/domain/Promotion.kt
@@ -0,0 +1,37 @@
+package christmas.domain
+
+import christmas.utils.Calendar
+
+class Promotion(private val promotionCalculation: PromotionCalculation, private val visitDate: Calendar) {
+    fun applyPromotion(): MutableMap<String, Int> {
+        val appliedPromotion = mutableMapOf<String, Int>()
+        applyChristmasAccount(appliedPromotion)
+        applyDayAccount(appliedPromotion)
+        applySpecialPromotion(appliedPromotion)
+        return appliedPromotion
+    }
+
+    fun applyChristmasAccount(appliedPromotion: MutableMap<String, Int>) {
+        if (visitDate.date <= 25) {
+            appliedPromotion["크리스마스 디데이 할인"] = promotionCalculation.christmasDiscount(visitDate.date)
+        }
+    }
+
+    fun applyDayAccount(appliedPromotion: MutableMap<String, Int>) {
+        if (isWeekend(visitDate.week)) {
+            appliedPromotion["주말 할인"] = promotionCalculation.weekendDiscount()
+            return
+        }
+        appliedPromotion["평일 할인"] = promotionCalculation.weekdayDiscount()
+    }
+
+    fun isWeekend(weekend: String): Boolean {
+        return weekend == "FRIDAY" || weekend == "SATURDAY"
+    }
+
+    fun applySpecialPromotion(appliedPromotion: MutableMap<String, Int>) {
+        if (visitDate.star) {
+            appliedPromotion["특별 할인"] = promotionCalculation.specialDiscount()
+        }
+    }
+}
