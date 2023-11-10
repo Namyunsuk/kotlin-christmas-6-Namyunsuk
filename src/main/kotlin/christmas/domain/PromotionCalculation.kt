06aeae4b58f1f143e9a5e649e7a378e60f92e1fa
Namyunsuk <yunsuk0328@naver.com>
2023-11-10 19:59:46 +0900

    feat(PromotionCalculation): 할인 별 할인금액 계산 기능 구현

---
+++ src/main/kotlin/christmas/domain/PromotionCalculation.kt
@@ -0,0 +1,33 @@
+package christmas.domain
+
+import christmas.utils.Menu
+
+class PromotionCalculation(private val orderedMenu: Map<Menu, Int>) {
+    fun christmasDiscount(date: Int): Int {
+        return INITIAL_AMOUNT + (date - START_DAY) * DISCOUNT_PER_DAY;
+    }
+
+    // 평일 할인
+    fun weekdayDiscount(): Int {
+        val dessertCount = orderedMenu.filter { it.key.category == "디저트" }.values.sum()
+        return dessertCount * DISCOUNT_PER_MENU
+    }
+
+    //주말 할인
+    fun weekendDiscount(): Int {
+        val mainCount = orderedMenu.filter { it.key.category == "메인" }.values.sum()
+        return mainCount * DISCOUNT_PER_MENU
+    }
+
+    fun specialDiscount(): Int {
+        return SPECIAL_DISCOUNT
+    }
+
+    companion object {
+        const val START_DAY = 1
+        const val INITIAL_AMOUNT = 1000
+        const val DISCOUNT_PER_DAY = 100
+        const val DISCOUNT_PER_MENU = 2023
+        const val SPECIAL_DISCOUNT = 1000
+    }
+}
