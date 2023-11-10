d59fb466913d0b7245df4bd624c4698323a88911
Namyunsuk <yunsuk0328@naver.com>
2023-11-11 01:17:31 +0900

    feat(MoneyCalculation): 금액 계산 기능 구현

---
+++ src/main/kotlin/christmas/domain/MoneyCalculation.kt
@@ -0,0 +1,23 @@
+package christmas.domain
+
+import christmas.utils.Menu
+
+class MoneyCalculation(private val appliedPromotion: MutableMap<String, Int>, private val orderedMenu: Map<Menu, Int>) {
+    fun calculateTotalPrice(): Int {
+        var totalPrice = 0
+        orderedMenu.forEach { (menu, count) -> totalPrice += menu.price * count }
+        return totalPrice
+    }
+
+    fun calculateTotalDiscount(): Int {
+        return appliedPromotion.filter { it.key != "증정 이벤트" }.values.sum()
+    }
+
+    fun calculateTotalPromotionPrice(): Int {
+        return appliedPromotion.values.sum()
+    }
+
+    fun calculateExpectedPayAmount(): Int {
+        return calculateTotalPrice() - calculateTotalDiscount()
+    }
+}
