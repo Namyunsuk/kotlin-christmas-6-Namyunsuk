5da82466df6b596d13d60188ddbe3b6024556382
Namyunsuk <yunsuk0328@naver.com>
2023-11-11 12:48:24 +0900

    refactor: 입력 기능 companion object내로 함수 이동

--- src/main/kotlin/christmas/view/InputView.kt
+++ src/main/kotlin/christmas/view/InputView.kt
@@ -3,18 +3,20 @@ package christmas.view
 import camp.nextstep.edu.missionutils.Console
 
 class InputView {
-    fun readDate(): Int {
-        println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
-        val input = Console.readLine()
-        return input.toInt()
-    }
+    companion object {
+        fun readDate(): Int {
+            println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
+            val input = Console.readLine()
+            return input.toInt()
+        }
 
-    fun readMenu(): Map<String, Int> {
-        println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
-        val input = Console.readLine()
-        val menu = input.split(",")
-            .map { it.split("-") }
-            .associate { (menu, count) -> menu to count.toInt() }
-        return menu
+        fun readMenu(): Map<String, Int> {
+            println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
+            val input = Console.readLine()
+            val menu = input.split(",")
+                .map { it.split("-") }
+                .associate { (menu, count) -> menu to count.toInt() }
+            return menu
+        }
     }
 }
